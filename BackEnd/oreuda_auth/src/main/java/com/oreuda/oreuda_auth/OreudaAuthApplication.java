060ac5f4a14cc7302184604ab818829910224c16
길상욱 <sanguk9805@naver.com>
2023-04-24 16:59:50 +0900

    :tada: start auth project

---
+++ BackEnd/oreuda_auth/src/main/java/com/oreuda/oreuda_auth/OreudaAuthApplication.java
@@ -0,0 +1,13 @@
+package com.oreuda.oreuda_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OreudaAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OreudaAuthApplication.class, args);
+	}
+
+}
---
+++ BackEnd/oreuda_auth/src/test/java/com/oreuda/oreuda_auth/OreudaAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oreuda.oreuda_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OreudaAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
