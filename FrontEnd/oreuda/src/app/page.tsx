3bb0553cc723f6f867ae8cda03d8843dc31fcb00
송은지 <cjsdks518@naver.com>
2023-04-26 16:11:16 +0900

    :sparkles: main upside part(github statistic)

--- FrontEnd/oreuda/src/app/page.tsx
+++ FrontEnd/oreuda/src/app/page.tsx
@@ -1,3 +1,14 @@
+import st from "./page.module.scss";
+import Statistic from "@/Component/Main/statistic";
+
 export default function Home() {
-  return <div>main</div>;
+  return (
+    <div className={st.body}>
+      <Statistic />
+      <div className={st.character}>
+        <div>캐릭터</div>
+        <div>그래프</div>
+      </div>
+    </div>
+  );
 }
