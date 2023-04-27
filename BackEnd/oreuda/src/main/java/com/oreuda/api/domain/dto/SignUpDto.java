99d384a425d8834769e6494325de7e866726c0ae
유지연 <jiyoenyou0416@naver.com>
2023-04-27 17:02:44 +0900

    :card_file_box: add UserController

---
+++ BackEnd/oreuda/src/main/java/com/oreuda/api/domain/dto/SignUpDto.java
@@ -0,0 +1,14 @@
+package com.oreuda.api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class SignUpDto {
+
+	String userId;
+	String image;
+}
