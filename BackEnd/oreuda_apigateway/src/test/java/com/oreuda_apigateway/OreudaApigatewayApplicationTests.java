7be900a622ab60814d5b7e8a5cdeb6da0f495dec
유지연 <jiyoenyou0416@naver.com>
2023-04-27 15:29:59 +0900

    :sparkles: add apigateway and eureka

---
+++ BackEnd/oreuda_apigateway/src/test/java/com/oreuda_apigateway/OreudaApigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oreuda_apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OreudaApigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
