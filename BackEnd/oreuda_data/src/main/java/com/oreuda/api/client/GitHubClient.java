98d3953ad68549216f502ec06e90dc6f0500235e
soyun9812 <soyun9812@naver.com>
2023-04-27 16:05:03 +0900

    :art: webclient graphql api

--- BackEnd/oreuda_data/src/main/java/com/oreuda/api/client/GitHubClient.java
+++ BackEnd/oreuda_data/src/main/java/com/oreuda/api/client/GitHubClient.java
@@ -1,45 +1,55 @@
 package com.oreuda.api.client;
 
-import java.nio.file.Files;
+import graphql.kickstart.spring.webclient.boot.GraphQLRequest;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.oreuda.api.domain.dto.RepositoryDto;
+import com.fasterxml.jackson.databind.JsonNode;
 
-import graphql.kickstart.execution.GraphQLRequest;
 import lombok.RequiredArgsConstructor;
 
+@Component
 @RequiredArgsConstructor
 public class GitHubClient {
 
 	private final static String GITHUB_URL = "https://api.github.com/graphql";
 	private final WebClient webClient;
-	private GraphQLRequest graphQLRequest;
 
 	public GitHubClient() {
+		System.out.println("GitHubClient!!!");
 		this.webClient = WebClient.builder()
 			.baseUrl(GITHUB_URL)
 			.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
 			.build();
 	}
 
-	public void getRepositories(String accessToken, int perPage, String cursor) {
-
-		webClient.post()
+	public JsonNode getRepositories(String accessToken, GraphQLRequest request) {
+		return webClient.post()
 			.uri(GITHUB_URL)
 			.header("Authorization", "Bearer " + accessToken)
-			// .body(BodyInserters.fromValue(GraphQLRequest.builder()))
+			.bodyValue(request.getRequestBody())
 			.retrieve()
-			.bodyToMono(RepositoryDto.class) // RepositoryDto API 응답을 파싱하는 클래스입니다.
-			.subscribe(response -> {
-				// 결과 처리 로직 작성
-				// response의 getViewer()를 호출하여 결과 데이터에 접근할 수 있습니다.
-			});
+			.bodyToMono(JsonNode.class)
+			.block()
+			.get("data")
+			.get("viewer");
+
+		// webClient.post()
+		// 	.uri(GITHUB_URL)
+		// 	.header("Authorization", "Bearer " + accessToken)
+		// 	// .body(BodyInserters.fromValue(graphQLRequest))
+		// 	.bodyValue(request.getRequestBody())
+		// 	.retrieve()
+		// 	.bodyToMono(JsonNode.class)
+		// 	.subscribe(response -> {
+		// 		if (response.get("data") == null) {
+		// 			// 예외 처리 로직
+		// 		}
+		// 	}, error -> {
+		// 		System.out.println("error@@@");
+		// 	});
 	}
 }
