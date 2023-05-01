c64d9489d8f8d0f913d2325cd09fb4b1f11755ca
soyun9812 <soyun9812@naver.com>
2023-05-01 17:58:01 +0900

    :art: add eureka

--- BackEnd/oreuda_data/src/main/java/com/oreuda/OreudaDataApplication.java
+++ BackEnd/oreuda_data/src/main/java/com/oreuda/OreudaDataApplication.java
@@ -2,8 +2,10 @@ package com.oreuda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
 @EnableFeignClients(basePackages = "com.oreuda.api.client")
 @SpringBootApplication
 public class OreudaDataApplication {
