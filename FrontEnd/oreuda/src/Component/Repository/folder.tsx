6bae58c309e7de569e386ab5fdddfdee9e4ec6e7
송은지 <cjsdks518@naver.com>
2023-05-02 17:39:15 +0900

    :sparkles: folder click move to repository list

--- FrontEnd/oreuda/src/Component/Repository/folder.tsx
+++ FrontEnd/oreuda/src/Component/Repository/folder.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 import Image from "next/image";
 
 import st from "./folder.module.scss";
 
 interface FolderList {
+  id: number;
   name: string;
   color: string;
   order: number;
@@ -11,114 +13,133 @@ interface FolderList {
 
 const folderList: FolderList[] = [
   {
+    id: 1,
     name: "FE",
     color: "blue",
     order: 1,
     repositoryCount: 23,
   },
   {
+    id: 2,
     name: "BE",
     color: "green",
     order: 2,
     repositoryCount: 3,
   },
   {
+    id: 3,
     name: "hello",
     color: "pink",
     order: 3,
     repositoryCount: 15,
   },
   {
+    id: 4,
     name: "Algo",
     color: "blue",
     order: 4,
     repositoryCount: 18,
   },
   {
+    id: 5,
     name: "몰라요",
     color: "pink",
     order: 5,
     repositoryCount: 18,
   },
   {
+    id: 6,
     name: "알려줘",
     color: "green",
     order: 6,
     repositoryCount: 18,
   },
   {
+    id: 7,
     name: "몰라요",
     color: "pink",
     order: 5,
     repositoryCount: 18,
   },
   {
+    id: 8,
     name: "알려줘",
     color: "green",
     order: 6,
     repositoryCount: 18,
   },
   {
+    id: 9,
     name: "몰라요",
     color: "pink",
     order: 5,
     repositoryCount: 18,
   },
   {
+    id: 10,
     name: "알려줘",
     color: "green",
     order: 6,
     repositoryCount: 18,
   },
   {
+    id: 11,
     name: "몰라요",
     color: "pink",
     order: 5,
     repositoryCount: 18,
   },
   {
+    id: 12,
     name: "알려줘",
     color: "green",
     order: 6,
     repositoryCount: 18,
   },
   {
+    id: 13,
     name: "몰라요",
     color: "pink",
     order: 5,
     repositoryCount: 18,
   },
   {
+    id: 14,
     name: "알려줘",
     color: "green",
     order: 6,
     repositoryCount: 18,
   },
   {
+    id: 15,
     name: "몰라요",
     color: "pink",
     order: 5,
     repositoryCount: 18,
   },
   {
+    id: 16,
     name: "알려줘",
     color: "green",
     order: 6,
     repositoryCount: 18,
   },
   {
+    id: 17,
     name: "몰라요",
     color: "pink",
     order: 5,
     repositoryCount: 18,
   },
   {
+    id: 18,
     name: "알려줘",
     color: "green",
     order: 6,
     repositoryCount: 18,
   },
   {
+    id: 19,
     name: "몰라요",
     color: "pink",
     order: 5,
@@ -126,26 +147,24 @@ const folderList: FolderList[] = [
   },
 ];
 
-const fakeBox: Array<number> = [];
-for (let i: number = 0; i < folderList.length % 5; i++) {
-  fakeBox.push(1);
-}
-
 export default function folder() {
   return (
     <div className={st.folders}>
       {folderList.map((e: FolderList) => {
         return (
-          <div key={e.order} className={st.folder}>
+          <Link
+            href={{
+              pathname: `/repository/${e.id}`,
+            }}
+            key={e.id}
+            className={st.folder}
+          >
             <div>
               <Image src={`images/repository/${e.color}.svg`} alt="폴더" width={128} height={128} />
             </div>
             <p>{e.name}</p>
-          </div>
+          </Link>
         );
-      })}
-      {fakeBox.map((e) => {
-        return <div key={e} className={st.fake} />;
       })}
     </div>
   );
