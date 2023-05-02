7292a7293757e72562393e570fda89afc2f042e4
유지연 <jiyoenyou0416@naver.com>
2023-05-02 12:58:28 +0900

    :bug: update DataClient

--- BackEnd/oreuda/src/main/java/com/oreuda/OreudaApplication.java
+++ BackEnd/oreuda/src/main/java/com/oreuda/OreudaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients(basePackages = "com.oreuda.client")
+@EnableFeignClients(basePackages = "com.oreuda.api.client")
 @EnableEurekaClient
 @SpringBootApplication
 public class OreudaApplication {
