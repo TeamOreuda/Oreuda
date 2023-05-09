90d2607c00f4409470fad6cea5717d0930cb68d5
길상욱 <sanguk9805@naver.com>
2023-05-09 10:49:55 +0900

    :art: fix login

--- BackEnd/oreuda_auth/src/main/java/com/oreuda/oreuda_auth/api/handler/OAuth2AuthenticationSuccessHandler.java
+++ BackEnd/oreuda_auth/src/main/java/com/oreuda/oreuda_auth/api/handler/OAuth2AuthenticationSuccessHandler.java
@@ -42,7 +42,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         log.info("로그인 성공");
-        log.info("request = {}", request.getRemoteHost());
+        log.info("request = {}", request.getRequestURL());
         // 사용자 정보
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         Map<String, Object> attributes = oAuth2User.getAttributes();
