72fb031b8cacae1cbd58cb511ac91d9d081bc819
길상욱 <sanguk9805@naver.com>
2023-05-09 14:04:21 +0900

    :art: log

--- BackEnd/oreuda_auth/src/main/java/com/oreuda/oreuda_auth/api/handler/OAuth2AuthenticationSuccessHandler.java
+++ BackEnd/oreuda_auth/src/main/java/com/oreuda/oreuda_auth/api/handler/OAuth2AuthenticationSuccessHandler.java
@@ -88,7 +88,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
             if (refresh != null && tokenProvider.validateToken(refresh) == JwtCode.ACCESS) {
                 // refresh token 그대로 사용
                 token = Token.builder().accessToken(access).refreshToken(refresh).build();
-                log.info("refresh token1: {}", refresh);
+//                log.info("refresh token1: {}", refresh);
             } else {
                 // refresh token 재발급
                 token = tokenProvider.generateToken(userDto.getUserId(), Role.USER.getKey());
@@ -97,7 +97,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
                         token.getRefreshToken(),
                         tokenProvider.getExpiration(TokenKey.REFRESH)
                 );
-                log.info("refresh token2: {}", token.getRefreshToken());
+//                log.info("refresh token2: {}", token.getRefreshToken());
             }
             saveGitHubTokenAndNodeId(String.valueOf(auth.getAuthId()), String.valueOf(attributes.get("nodeId")), String.valueOf(attributes.get("accessToken")));
         }
