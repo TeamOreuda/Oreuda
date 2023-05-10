f151db839660d25d2b18ca28c43988eecbda5d20
길상욱 <sanguk9805@naver.com>
2023-05-10 14:54:18 +0900

    :art: fix login

--- FrontEnd/oreuda/src/app/login/page.tsx
+++ FrontEnd/oreuda/src/app/login/page.tsx
@@ -3,9 +3,7 @@ import Link from "next/link";
 export default function Login() {
   return (
     <div>
-      <Link href="http://52.79.221.133:8090/oauth2/authorization/github">
-        login
-      </Link>
+      <Link href="http://52.79.221.133:8090/oauth2/authorization/github">login</Link>
     </div>
   );
 }
