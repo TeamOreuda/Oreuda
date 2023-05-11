25e1019f4c617fbc107a1cc723cba69a576487db
송은지 <cjsdks518@naver.com>
2023-05-11 14:21:00 +0900

    :sparkles: connect axios folderList

--- FrontEnd/oreuda/src/Api/Folders/getFolderList.ts
+++ FrontEnd/oreuda/src/Api/Folders/getFolderList.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const GetFolderList = async (ACCESS_TOKEN: any) => {
-  return await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/folder`, {
+export const GetFolderList = (ACCESS_TOKEN: any) => {
+  return axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/folder`, {
     headers: {
       Authorization: ACCESS_TOKEN,
     },
