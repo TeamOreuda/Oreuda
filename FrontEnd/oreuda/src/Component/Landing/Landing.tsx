ce769d332680d31fb61f437bfff4a0dc94eaffbc
김지환 <tykimdream@naver.com>
2023-05-15 17:12:53 +0900

    :bug: spelling error
    spelling error

--- FrontEnd/oreuda/src/Component/Landing/Landing.tsx
+++ FrontEnd/oreuda/src/Component/Landing/Landing.tsx
@@ -1,17 +1,13 @@
 import st from "./Landing.module.scss";
 
-import LandingHeader from "./LandingHeader";
-import LandingRepo from "./LandingRepo";
-import PageFormat from "./PageFormat";
-
-import Main1 from "./Main1";
+import Main1 from "./Mains/Main1";
+import Main2 from "./Mains/Main2";
 
 export default function Landing() {
   return (
     <div className={st.page}>
-      {/* <LandingHeader />
-      <PageFormat /> */}
       <Main1 />
+      {/* <Main2 /> */}
     </div>
   );
 }
