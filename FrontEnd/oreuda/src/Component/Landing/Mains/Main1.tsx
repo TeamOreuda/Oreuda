ee639ce3af9a275733a9f0f0197355cf7aa9e43f
unknown <tykimdream@gmail.com>
2023-05-16 23:45:17 +0900

    :zap: Main 4 ing
    main 4 ing

--- FrontEnd/oreuda/src/Component/Landing/Mains/Main1.tsx
+++ FrontEnd/oreuda/src/Component/Landing/Mains/Main1.tsx
@@ -52,7 +52,7 @@ export default function Main1() {
               <Image
                 data-position={1}
                 data-name={1}
-                src={`images/landing/main1/Soil.svg`}
+                src={`/images/landing/main1/Soil.svg`}
                 alt="폴더"
                 width={35}
                 height={32}
@@ -69,7 +69,7 @@ export default function Main1() {
                   <Image
                     data-position={1}
                     data-name={1}
-                    src={`images/landing/main1/Github.svg`}
+                    src={`/images/landing/main1/Github.svg`}
                     alt="폴더"
                     width={44}
                     height={44}
@@ -89,7 +89,7 @@ export default function Main1() {
                   <Image
                     data-position={1}
                     data-name={1}
-                    src={`images/landing/main1/Google.svg`}
+                    src={`/images/landing/main1/Google.svg`}
                     alt="폴더"
                     width={44}
                     height={44}
@@ -108,7 +108,7 @@ export default function Main1() {
             <Image
               data-position={1}
               data-name={1}
-              src={`images/landing/main1/Cloud.svg`}
+              src={`/images/landing/main1/Cloud.svg`}
               alt="구름"
               width={600}
               height={500}
@@ -119,7 +119,7 @@ export default function Main1() {
             <Image
               data-position={1}
               data-name={1}
-              src={`images/landing/main1/Sun.svg`}
+              src={`/images/landing/main1/Sun.svg`}
               alt="해"
               width={500}
               height={500}
@@ -131,7 +131,7 @@ export default function Main1() {
             <Image
               data-position={1}
               data-name={1}
-              src={`images/landing/main1/Cloud.svg`}
+              src={`/images/landing/main1/Cloud.svg`}
               alt="구름"
               width={600}
               height={500}
@@ -144,7 +144,7 @@ export default function Main1() {
               <Image
                 data-position={1}
                 data-name={1}
-                src={`images/landing/main1/Mountain.svg`}
+                src={`/images/landing/main1/Mountain.svg`}
                 alt="산"
                 width={1200}
                 height={800}
@@ -155,7 +155,7 @@ export default function Main1() {
               <Image
                 data-position={1}
                 data-name={1}
-                src={`images/landing/main1/Mountain_close.svg`}
+                src={`/images/landing/main1/Mountain_close.svg`}
                 alt="산"
                 width={1200}
                 height={800}
